815fc5a0d4a873c96e3521f061c8956210003ccb
Aleksi <aleksi1.deda@gmail.com>
2022-04-01 16:32:41 +0200

    creating model and form for posts

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import CASCADE
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title + "/n" + self.description
+
+
